3e610541eb5614f884300e4435b473c7a5bd9812
Arxae <maesenandy@gmail.com>
2015-10-16 13:02:26 +0200

    Added Singleton

---
+++ ArxUtils/ArxUtils/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace ArxUtils
+{
+	public class Singleton<T> where T : new()
+	{
+		public static T Instance => Inner.InnerInstance;
+		private Singleton() { }
+
+		private class Inner
+		{
+			internal static T InnerInstance = new T();
+			static Inner() { }
+		}
+	}
+}
